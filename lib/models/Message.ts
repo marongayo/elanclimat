71bff3ba1e1467226cb4b6def582ce4dcc0c0b00
Mar Ongayo <sir.ongayo@gmail.com>
2026-05-11 12:33:34 +0300

    Update Message.ts

--- lib/models/Message.ts
+++ lib/models/Message.ts
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 
-const MessageSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: String,
-  service: String,
-  message: String,
-  date: { type: String, required: true },
-  read: { type: Boolean, default: false },
-  archived: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-});
+const MessageSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: String,
+    service: String,
+    message: String,
+    read: { type: Boolean, default: false },
+    archived: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true, // auto-manages createdAt + updatedAt
+  }
+);
 
 export const MessageModel =
   mongoose.models.Message || mongoose.model("Message", MessageSchema);
