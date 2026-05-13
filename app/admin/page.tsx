69961cf7f928a2f95b0eaf0d4426a878158ba766
Mar Ongayo <sir.ongayo@gmail.com>
2026-05-13 18:39:03 +0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,19 @@
 // admin/page.tsx
 
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import AdminClient from "./AdminClient";
 import { getBlogPosts, getProducts } from "@/lib/db";
 
-export const dynamic = "force-dynamic";
 
 export default async function AdminPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+
   const posts = await getBlogPosts();
   const products = await getProducts();
   return (
