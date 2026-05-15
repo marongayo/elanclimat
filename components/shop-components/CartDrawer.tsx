50c4740478e3bb4fdd75092ce59bd1241cba8f70
Mar Ongayo <sir.ongayo@gmail.com>
2026-05-15 19:24:26 +0300

    this

--- components/shop-components/CartDrawer.tsx
+++ components/shop-components/CartDrawer.tsx
@@ -185,6 +185,7 @@ export function CartDrawer({
                         }}
                       >
                         {item.name}
+                        {/* <span> */}
                       </div>
                       <div
                         style={{
